a486e8caec1402abd989fddbdd999c1f8e468d79
Leonardo Cunha <leonardocunha.a@gmail.com>
2024-09-17 20:37:11 -0300

    Refactoring and creation of a validation test for valid levels when creating a new npc and another test for invalid levels

--- tests/test_npc.py
+++ tests/test_npc.py
@@ -324,40 +324,47 @@ class TestNpc:
         assert npc1.category == expected_category_npc1
     
     def test_create_npc_with_level_between_0_and_100(self):
+        #expected
+        expected_name_npc1 = 'Ash'
+        expected_gender_npc1 = 'Male'
+        expected_age_npc1 = 20
+        expected_category_npc1 = 'Trainer'
+        expected_pokemons_npc1 = ['Pikachu', 'Bulbassaur', 'Charmander', 'Squirtle', 'Pidgeot', 'Butterfree']
+        expected_level_npc1 = 12
+
+        #when
+        npc1 = NPC(name = self.name_npc1, gender = self.gender_npc1, age = self.age_npc1, category = self.category_npc1, pokemons = self.pokemons_npc1, level = self.level_npc1)
+
+        #them
+        assert npc1.name == expected_name_npc1
+        assert npc1.gender == expected_gender_npc1
+        assert npc1.age == expected_age_npc1
+        assert npc1.category == expected_category_npc1
+        assert npc1.pokemons == expected_pokemons_npc1
+        assert npc1.checking_valid_level(self.level_npc1) == expected_level_npc1
+
+    def test_create_npc_with_invalid_level(self):
         #given
-        name_npc1 = 'Ash'
-        gender_npc1 = 'Male'
-        age_npc1 = 101
-        category_npc1 = 'Trainer'
-        pokemons_npc1 = ['Pikachu', 'Bulbassaur', 'Charmander', 'Squirtle', 'Pidgeot', 'Butterfree']
         level_npc1 = 105
 
         #expected
         expected_name_npc1 = 'Ash'
         expected_gender_npc1 = 'Male'
-        expected_age_npc1 = 'Invalid'
+        expected_age_npc1 = 20
         expected_category_npc1 = 'Trainer'
         expected_pokemons_npc1 = ['Pikachu', 'Bulbassaur', 'Charmander', 'Squirtle', 'Pidgeot', 'Butterfree']
         expected_level_npc1 = 'Invalid'
 
         #when
-        npc1 = NPC(name = name_npc1, gender = gender_npc1, age = age_npc1, category = category_npc1, pokemons = pokemons_npc1, level = level_npc1)
+        npc1 = NPC(name = self.name_npc1, gender = self.gender_npc1, age = self.age_npc1, category = self.category_npc1, pokemons = self.pokemons_npc1, level = level_npc1)
 
         #them
-        result_name_npc1 = npc1.name
-        result_gender_npc1 = npc1.gender
-        result_age_npc1 = npc1.age
-        result_category_npc1 = npc1.category
-        result_pokemons_npc1 = npc1.pokemons
-        result_level_npc1 = npc1.level
-
-        #assert
-        assert result_name_npc1 == expected_name_npc1
-        assert result_gender_npc1 == expected_gender_npc1
-        assert result_age_npc1 == expected_age_npc1
-        assert result_category_npc1 == expected_category_npc1
-        assert result_pokemons_npc1 == expected_pokemons_npc1
-        assert result_level_npc1 == expected_level_npc1
+        assert npc1.name == expected_name_npc1
+        assert npc1.gender == expected_gender_npc1
+        assert npc1.age == expected_age_npc1
+        assert npc1.category == expected_category_npc1
+        assert npc1.pokemons == expected_pokemons_npc1
+        assert npc1.checking_valid_level(level_npc1) == expected_level_npc1
     
     def test_create_npc_with_pokemons_between_0_and_6(self):
         #given
